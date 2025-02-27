63fbcb70cbacbada44a3ed9c5ff1e93956237d3b
Vince Buffalo <vsbuffalo@gmail.com>
2025-02-27 11:58:17 -0800

    Alpha release.

--- src/slimdemes/__init__.py
+++ src/slimdemes/__init__.py
@@ -1,4 +1,7 @@
+"""SLiM and demes integration package."""
 from .main import convert as convert_demes_to_json
 from .main import run_msprime as run_demes_msprime
 
-__all__ = ["convert_demes_to_json", "run_demes_msprime"]
+from ._version import __version__
+
+__all__ = ["convert_demes_to_json", "run_demes_msprime", "__version__"]
